2d8d4e8ce32a895024a7c7dd724b2ff547d2734f
Alyson Trizotto <79091765+AlysonTrizotto@users.noreply.github.com>
2025-04-19 00:55:32 -0300

    correção de log

--- app/Services/v1/UsersServices.php
+++ app/Services/v1/UsersServices.php
@@ -121,7 +121,7 @@ class UsersServices
             'email' => ['required', 'email'],
         ]);
 
-        $this->createLog('User reset password', 'email: ' .  $request->only(['email']));
+        $this->createLog('User reset password', 'email: ' .   json_encode($request->only(['email'])));
 
         // We will send the password reset link to this user. Once we have attempted
         // to send the link, we will examine the response then see the message we
